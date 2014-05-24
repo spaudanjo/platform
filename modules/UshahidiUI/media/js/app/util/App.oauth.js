3f58ed4bf4125458ba1bf0414ea4f6222cda1e42
Woody Gilk <woody@ushahidi.com>
2014-05-24 11:13:39 -0500

    remove user provider from oauth.js

--- modules/UshahidiUI/media/js/app/util/App.oauth.js
+++ modules/UshahidiUI/media/js/app/util/App.oauth.js
@@ -14,10 +14,7 @@ define(['backbone', 'jso2/jso2', 'jquery', 'underscore'],
 			initialize : function ()
 			{
 				// this treats the user secret for the ui client as a pubkey
-				var user_client = $.cookie('oauthclient'),
-					user_secret = $.cookie('oauthsecret'),
-					token = Jso2.store.getToken('ushahidi_user'),
-					anonymous_config = {
+				var anonymous_config = {
 						client_id: window.config.oauth.client,
 						client_secret: window.config.oauth.client_secret,
 						// no authorization required for client_credentials
@@ -28,10 +25,9 @@ define(['backbone', 'jso2/jso2', 'jquery', 'underscore'],
 							require: ['posts', 'media', 'forms', 'api', 'tags', 'sets', 'users']
 						},
 						grant_type: 'client_credentials'
-					},
-					user_config = {};
+					};
 
-				_.bindAll(this, 'setProvider', 'logout', 'ajax');
+				_.bindAll(this, 'setProvider', 'ajax');
 
 				Jso2.enablejQuery($);
 
@@ -39,26 +35,14 @@ define(['backbone', 'jso2/jso2', 'jquery', 'underscore'],
 				this.provider = null;
 
 				this.providers.anonymous = new Jso2('ushahidi_anonymous', anonymous_config);
-
-				if (user_secret)
-				{
-					user_config = _.extend({}, anonymous_config, {
-						client_id: user_client,
-						client_secret: user_secret
-						// todo: user should have different scoping
-						});
-					this.providers.user = new Jso2('ushahidi_user', user_config);
-				}
-
-				// If user credentials are not available, use anonymous access.
-				this.setProvider((user_secret && token) ? 'user' : 'anonymous');
+				this.setProvider('anonymous');
 
 				// Override backbone AJAX with our AJAX switcher
 				Backbone.ajax = this.ajax;
 			},
 			/**
 			 * Set OAuth Provider
-			 * @param {String} provider_name Provider name: client_credentials or implicit
+			 * @param {String} provider_name Provider name: anonymous
 			 */
 			setProvider : function(provider_name)
 			{
@@ -80,18 +64,6 @@ define(['backbone', 'jso2/jso2', 'jquery', 'underscore'],
 					headers.Authorization = 'Bearer ' + this.currentToken.access_token;
 				}
 				return headers;
-			},
-			/**
-			 * Logout: Switch back to anonymous
-			 */
-			logout : function ()
-			{
-				var xhr = this.setProvider('anonymous');
-				this.currentToken = null;
-				this.providers.user.wipeTokens();
-				// Redirect to /user/logout
-				window.location = window.config.baseurl + 'user/logout?from_url=/';
-				return xhr;
 			},
 			/**
 			 * Call the appropriate ajax function based on provider
