4589694804b4ed3acec40d6bed0ee0aa0aee6eb9
Daniel Spaude <daniel@spaude.de>
2014-07-30 20:14:40 +0200

    add migration for alert_subscriptions

---
+++ application/migrations/3-0/20140730174616.php
@@ -0,0 +1,31 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Migration_3_0_20140730174616 extends Minion_Migration_Base {
+
+	/**
+	 * Run queries needed to apply this migration
+	 *
+	 * @param Kohana_Database $db Database connection
+	 */
+	public function up(Kohana_Database $db)
+	{
+		$db->query(NULL, "CREATE TABLE IF NOT EXISTS `alert_subscriptions` (
+			`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
+			`name` VARCHAR(150) NOT NULL DEFAULT '' ,
+			PRIMARY KEY (`id`)
+			)
+		ENGINE = InnoDB
+		DEFAULT CHARACTER SET = utf8;");
+	}
+
+	/**
+	 * Run queries needed to remove this migration
+	 *
+	 * @param Kohana_Database $db Database connection
+	 */
+	public function down(Kohana_Database $db)
+	{
+		$db->query(NULL, 'DROP TABLE `alert_subscriptions`');
+	}
+
+}
